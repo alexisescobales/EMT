e92bbca2f33044e842964f9c5c2d1973d92fdd92
feretto <facundocapurata@gmail.com>
2023-03-20 13:31:26 +0100

    Add scss and bootstrap

--- EMT/resources/views/index.blade.php
+++ EMT/resources/views/index.blade.php
@@ -3,52 +3,5 @@
 @section('title', 'Emergency Medical Team')
 
 @section('content')
-<div class="row">
-    <div class="col-8" style="background-color: rgb(197, 246, 61)">
-        {{-- Contenido de la carta --}}
-        <div class="card">
-        <div class="card-header">
-            Quote
-        </div>
-        <div class="card-body">
-            <blockquote class="blockquote mb-0">
-            <p>A well-known quote, contained in a blockquote element.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-            </blockquote>
-        </div>
-        </div>
-    </div>
 
-    <div class="col-4 d-flex flex-column" style="background-color: green">
-        <div>
-            {{-- Contenido de los expedientes --}}
-            <div class="card">
-            <div class="card-header">
-                Quote
-            </div>
-            <div class="card-body">
-                <blockquote class="blockquote mb-0">
-                <p>A well-known quote, contained in a blockquote element.</p>
-                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                </blockquote>
-            </div>
-            </div>
-        </div>
-
-        <div>
-            {{-- Contenido del mapa --}}
-            <div class="card">
-            <div class="card-header">
-                Quote
-            </div>
-            <div class="card-body">
-                <blockquote class="blockquote mb-0">
-                <p>A well-known quote, contained in a blockquote element.</p>
-                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-                </blockquote>
-            </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
